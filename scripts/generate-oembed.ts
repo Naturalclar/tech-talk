43831e23efc98546366beaf74e034204c6c54dcc
jesse <jesse.katsumata@cureapp.jp>
2019-05-24 15:36:33 +0900

    :pencil: edit url

--- scripts/generate-oembed.ts
+++ scripts/generate-oembed.ts
@@ -4,12 +4,12 @@ const [dir] = process.argv.slice(2)
 const oEmbed = {
   type: 'rich',
   version: '1.0',
-  provider_name: 'slides.naturalclar.com',
-  provider_url: 'https://slides.naturalclar.com',
+  provider_name: 'slides.naturalclar.dev',
+  provider_url: 'https://slides.naturalclar.dev',
   title: '',
   width: 658,
   height: 408,
-  html: `<iframe style="width: 100%; overflow: hidden;" src="https://slides.naturalclar.com/${dir}/index.html" width="658" height="408" frameborder="0" scrolling="no" ></iframe>`,
+  html: `<iframe style="width: 100%; overflow: hidden;" src="https://slides.naturalclar.dev/${dir}/index.html" width="658" height="408" frameborder="0" scrolling="no" ></iframe>`,
 }
 
 console.log(JSON.stringify(oEmbed))
--- src/components/Meta.js
+++ src/components/Meta.js
@@ -2,18 +2,26 @@ import React from 'react'
 
 // taken from https://github.com/Leko/slides
 
-const Meta = ({ title, locale = 'ja_JP', publishedAt, slug = title }) => (
+const Meta = ({
+  title,
+  description,
+  locale = 'ja_JP',
+  publishedAt,
+  slug = title,
+}) => (
   <>
+    <meta name="twitter:description" content={description} />
+    <meta property="og:description" content={description} />
     <meta property="og:locale" content={locale} />
     <meta property="og:title" content={title} />
     <meta property="og:type" content="article" />
     <meta
       property="og:url"
-      content={`https://slides.naturalclar.com/${slug}/`}
+      content={`https://slides.naturalclar.dev/${slug}/`}
     />
     <meta
       property="og:image"
-      content={`https://slides.naturalclar.com/${slug}.png`}
+      content={`https://slides.naturalclar.dev/${slug}.png`}
     />
     <meta
       property="article:published_time"
@@ -24,9 +32,15 @@ const Meta = ({ title, locale = 'ja_JP', publishedAt, slug = title }) => (
     <meta name="twitter:title" content={title} />
     <meta
       name="twitter:image"
-      content={`https://slides.naturalclar.com/${slug}.png`}
+      content={`https://slides.naturalclar.dev/${slug}.png`}
     />
     <title>{title}</title>
+    <link
+      rel="alternate"
+      type="application/json+oembed"
+      href={`https://slides.naturalclar.dev/${slug}/oembed.json`}
+      title={title}
+    />
   </>
 )
 
